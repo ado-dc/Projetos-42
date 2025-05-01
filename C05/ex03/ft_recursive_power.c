4fd1a27be218a7f9f81a5ec9ecd977f6e06a8a02
ado-dc <josecassule631@gmail.com.com>
2025-05-01 16:53:37 +0100

    feito

--- C05/ex03/ft_recursive_power.c
+++ C05/ex03/ft_recursive_power.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ado-dc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 16:46:29 by ado-dc            #+#    #+#             */
+/*   Updated: 2025/05/01 16:49:24 by ado-dc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_recursive_power(int nb, int power)
 {
 	if (nb == 0 && power == 0)
 		return (1);
 	if (power == 0)
-		return 1;
+		return (1);
 	if (power > 0)
 		return (nb * ft_recursive_power(nb, power - 1));
 	return (0);
