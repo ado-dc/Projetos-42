a297edb46d25a7308dabdb8dcd1de7a1dadf87d0
ado-dc <josecassule631@gmail.com.com>
2025-05-15 23:17:17 +0100

    feito

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ado-dc <ado-dc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 15:44:11 by ado-dc            #+#    #+#             */
-/*   Updated: 2025/05/15 16:15:45 by ado-dc           ###   ########.fr       */
+/*   Updated: 2025/05/15 23:04:46 by ado-dc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,40 @@
 
 static int	ft_nbrlen(int n)
 {
-	int				len;
-	unsigned int	nb;
+	int	len;
 
-	len = 0;
-	if (n <= 0)
+	len = 1;
+	while (n / 10)
 	{
-		len++;
-		nb = (unsigned int)(-((long)n));
-	}
-	else
-		nb = (unsigned int)n;
-	while (nb > 0)
-	{
-		nb /= 10;
+		n /= 10;
 		len++;
 	}
+	if (n < 0)
+		len++;
 	return (len);
 }
 
 char	*ft_itoa(int n)
 {
-	char			*str;
-	int				len;
-	unsigned int	nb;
+	char	*str;
+	int		len;
+	long	nb;
 
+	nb = n;
 	len = ft_nbrlen(n);
-	str = (char *)malloc(sizeof(char) * (len + 1));
+	str = (char *)malloc(len + 1);
 	if (!str)
 		return (NULL);
 	str[len] = '\0';
-	if (n < 0)
-		nb = (unsigned int)(-((long)n));
-	else
-		nb = (unsigned int)n;
-	while (len--)
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (--len >= 0 && str[len] != '-')
 	{
 		str[len] = (nb % 10) + '0';
 		nb /= 10;
-		if (n < 0 && len == 0)
-			str[0] = '-';
 	}
 	return (str);
 }
